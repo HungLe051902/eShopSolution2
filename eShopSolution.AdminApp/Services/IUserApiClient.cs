ccbc9715d9a374b7eb9d59860028c57ee54f8537
HungLe051902 <43202901+HungLe051902@users.noreply.github.com>
2021-02-28 23:04:27 +0700

    Commit

--- eShopSolution.AdminApp/Services/IUserApiClient.cs
+++ eShopSolution.AdminApp/Services/IUserApiClient.cs
@@ -12,5 +12,7 @@ namespace eShopSolution.AdminApp.Services
         Task<string> Authenticate(LoginRequest request);
 
         Task<PagedResult<UserVm>> GetUsersPaging(GetUserPagingRequest request);
+
+        Task<bool> RegisterUser(RegisterRequest registerRequest);
     }
 }
