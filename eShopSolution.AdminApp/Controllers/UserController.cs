97a70b05f884c09ad8897f3f9c54d195edc80f74
HungLe051902 <43202901+HungLe051902@users.noreply.github.com>
2021-03-07 16:04:19 +0700

    Commit

--- eShopSolution.AdminApp/Controllers/UserController.cs
+++ eShopSolution.AdminApp/Controllers/UserController.cs
@@ -36,6 +36,9 @@ namespace eShopSolution.AdminApp.Controllers
                 PageSize = pageSize
             };
             var data = await _userApiClient.GetUsersPaging(request);
+            ViewBag.Keyword = keyword;
+            if (TempData["result"] != null)
+                ViewBag.SuccessMsg = TempData["result"];
             return View(data.ResultObj);
         }
 
@@ -59,7 +62,10 @@ namespace eShopSolution.AdminApp.Controllers
                 return View();
             var result = await _userApiClient.RegisterUser(request);
             if (result.IsSuccessed)
+            {
+                TempData["result"] = "Thêm mới người dùng thành công";
                 return RedirectToAction("Index");
+            }
 
             ModelState.AddModelError("", result.Message);
             return View(request);
@@ -93,7 +99,10 @@ namespace eShopSolution.AdminApp.Controllers
                 return View();
             var result = await _userApiClient.UpdateUser(request.Id, request);
             if (result.IsSuccessed)
+            {
+                TempData["result"] = "Cập nhật người dùng thành công";
                 return RedirectToAction("Index");
+            }
 
             ModelState.AddModelError("", result.Message);
             return View(request);
@@ -122,7 +131,10 @@ namespace eShopSolution.AdminApp.Controllers
                 return View();
             var result = await _userApiClient.Delete(request.Id);
             if (result.IsSuccessed)
+            {
+                TempData["result"] = "Xóa người dùng thành công";
                 return RedirectToAction("Index");
+            }    
 
             ModelState.AddModelError("", result.Message);
             return View(request);
